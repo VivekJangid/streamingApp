9373bb5b1235cfaaad1977bbdea5646df0a7c531
vivekjangid <vivekjangid608@gmail.com>
2020-12-02 00:15:00 +0530

    Adding ROoutes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,30 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne <Link to="/pagetwo">Go To Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">Go To Page One</Link>
-    </div>
-  );
-};
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/streamCreate";
+import StreamDelete from "./streams/streamDelete";
+import StreamEdit from "./streams/streamEdit";
+import StreamList from "./streams/streamList";
+import StreamShow from "./streams/streamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
